92831e3d021294c2a2cab466e4d6f31981d249e7
Liangyongqi <1433044682@qq.com>
2021-01-16 09:40:15 +0800

    添加项目文件。

---
+++ test20210116/ParameterOpt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test20210116
+{
+    class ParameterOpt
+    {
+        public void testConsole()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test20210116/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test20210116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterOpt parameterOpt = new ParameterOpt();
+            parameterOpt.testConsole();
+        }
+    }
+}
